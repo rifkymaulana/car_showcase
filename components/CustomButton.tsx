73645d4504cf9811ffec0263d25b2c0ff8891002
rifkymaulana <rifkymaulanaindonesia@gmail.com>
2023-09-25 10:46:36 +0700

    feat: get api, filter data from api features

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import {CustomButtonProps} from "@/types";
 
 // @ts-ignore
-const CustomButton = ({ title, containerStyles, handleClick, btnType }): CustomButtonProps => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }): CustomButtonProps => {
     // @ts-ignore
     return (
         <button
@@ -13,9 +13,15 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType }): CustomB
          className={`custom-btn ${containerStyles}`}
          onClick={handleClick}
         >
-            <span className={`flex-1`}>
+            <span className={`flex-1 ${textStyles}`}>
                 {title}
             </span>
+
+            {rightIcon && (
+                <div className="relative w-6 h-6">
+                    <Image src={rightIcon} alt="right icon" fill className="object-contain" />
+                </div>
+            )}
         </button>
     )
 }
